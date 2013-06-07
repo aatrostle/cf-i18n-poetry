19b9988425fec24816420fbb79d300b24be1b1b0
Aaron Trostle <aaron.trostle@gmail.com>
2013-06-07 01:01:11 -0700

    Add custome time format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 I18nPoetry::Application.initialize!
+
+Time::DATE_FORMATS[:created_time] = "%A %B %d at %X"
