9d410ca8e62bbda2cc1aea01c3c6ba3a3f40b794
Aaron Trostle <aaron.trostle@gmail.com>
2013-06-06 23:49:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+I18nPoetry::Application.config.secret_token = '97c3051ae8b699e083648cb1de95fc66b362c39f145b263ccba50812af0aa05231e0b9f9536ecb7588ad19294ea4bf349591514d8075e589ad3033a257d7b7f0'
